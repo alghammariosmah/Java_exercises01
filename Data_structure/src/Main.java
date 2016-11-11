9f056410cc4fdd83bc929706b2d6f658fd9bb486
Sam <alghammariosamah@gmail.com>
2016-11-11 13:46:19 +0100

    Data Structrue (Nodes)

---
+++ Data_structure/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args){
+        List l = new List();
+        l.append(1);
+        l.append(2);
+        System.out.println(l);
+        l.append(3);
+        l.append(4);
+        System.out.println(l);
+        
+        
+    }
+
+}
