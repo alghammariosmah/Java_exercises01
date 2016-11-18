a8c78e30c7a2839a57dbc0f0aa67bd44d9c9dbda
Sam <alghammariosamah@gmail.com>
2016-11-18 11:53:05 +0100

    Binary Search tree adjust the Tree method

--- BinarySearchTree/src/Main.java
+++ BinarySearchTree/src/Main.java
@@ -4,14 +4,16 @@
 public class Main {
     public static void main(String[] args){
         BinarySearchTree t = new BinarySearchTree();
-        t.add("1");
-        t.add("2");
-        t.add("3");
-        t.add("4");
+        t.add("10");
         t.add("5");
-        System.out.println(t.getSize());
-        System.out.println(t);
-        System.out.println(t.getDepth());
-//        System.out.println(t.toStringAsTree());
+        t.add("15");
+        t.add("3");
+        t.add("8");
+        t.add("12");
+        t.add("20");
+//        System.out.println(t.getSize());
+//        System.out.println(t);
+//        System.out.println(t.getDepth());
+        System.out.println(t.toStringAsTree());
     }
 }
